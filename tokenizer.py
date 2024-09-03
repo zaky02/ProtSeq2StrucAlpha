88304c24a0c4aa8dceee1d7addca77f2a2f545ce
phastos <none>
2024-09-03 15:09:24 +0200

    finished in-house tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,11 @@
+import torch
 import itertools
 from SaProt.utils.foldseek_util import get_struc_seq
 
 foldseek_seq_vocab = "ACDEFGHIKLMNPQRSTVWY#"
 foldseek_struc_vocab = "pynwrqhgdlvtmfsaeikc#"
+# max_length refers to aa sequence length no to input length
+# with cls and eos the input max size/length is 1026 (+2)
 max_length = 1024
 
 foldseek_path = '/home/phastos/Programs/mambaforge/envs/SaProt/lib/python3.10/site-packages/SaProt/bin/foldseek'
@@ -13,7 +16,6 @@ class SaProtTokenizer:
         self.pad_token = '<pad>'
         self.unk_token = '<unk>'
         self.eos_token = '<eos>'
-        # It is being use? How so? There are many different ways of masking
         self.mask_token = '<mask>'
         self.build_vocab()
 
@@ -49,18 +51,35 @@ class SaProtTokenizer:
         input_ids = []
         attention_masks = []
 
-        for pdb in pdb_list:
-            seq, foldseek, combined = self.get_sa_vocab(pdb)
+        sas = [self.get_sa_vocab(pdb)[2] for pdb in pdbs]
+        # add 2 to the lonegst to account for cls and eos
+        longest = int(max(len(c) for c in sas)/2) + 2
 
-        if truncation and len(combined) > max_length -2: 
-        # max_length -2 to take into account the cls and eos tokens
-            combined = combined[:max_length - 2]
+        for sa in sas:
+            sa_list = [sa[i:i+2] for i in range(0, len(sa), 2)]
 
-        combined = list(combined)
-        combined = [self.cls_token] + combined + [self.eos_token]
-        print(combined)
+            # Truncation startegy for max_length (not longest)
+            if truncation and len(sa_list) > max_length: 
+                sa_list = sa_list[:max_length]
+                longest = len(sa_list)
             
-        
+            sa_list = [self.cls_token] + sa_list + [self.eos_token]
+            
+            # Padding strategy longest
+            if padding and len(sa_list) < longest:
+                sa_list = sa_list + [self.pad_token] * (longest - len(sa_list))
+            
+            input_id = [self.token2id[token] for token in sa_list]
+            input_ids.append(input_id)
+
+            attention_mask = [1 if token != self.pad_token else 0 for token in sa_list]
+            attention_masks.append(attention_mask)
+
+        input_ids_tensor = torch.tensor(input_ids)
+        attention_masks_tensor = torch.tensor(attention_masks)
+
+        return {'input_ids':input_ids_tensor, 'attention_mask':attention_masks_tensor}
+
     def get_sa_vocab(self, pdb_path, chain_id='A'):
         parsed_seqs = get_struc_seq(foldseek_path, pdb_path)[chain_id]
         seq, foldseek,  combined = parsed_seqs
@@ -72,4 +91,5 @@ if __name__ == "__main__":
     structures_directory = 'structures/'
     pdbs = glob.glob('%s*.pdb'%structures_directory)
     tokenizer = SaProtTokenizer()
-    tokenizer(pdbs)
+    inputs = tokenizer(pdbs, max_length=10)
+    print(inputs)
