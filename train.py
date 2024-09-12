6e224bfcb7171297b2098ef894ec9435e8e93030
IFilella <isaacfilella@gmail.com>
2024-09-12 11:15:34 +0200

    start building train.py

--- train.py
+++ train.py
@@ -1,69 +1,62 @@
 import json
 import torch
-import tensorflow as tf
-from model import create_encoder, create_decoder
+import random
+from utils.timer import Timer
 
 # Load hyperparameters from JSON file
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
+"""
 input_dim = config["input_dim"]
 output_dim = config["output_dim"]
 embedding_dim = config["embedding_dim"]
 units = config["units"]
 batch_size = config["batch_size"]
 epochs = config["epochs"]
+learning_rate = config["learning_rate"]
+"""
+"""
+# TO DO: apply masking non-randomly by introducing information
+# about attention values of the words and mask accordingly. Also, 
+# base the masking on pLDDT as found in the SaProt github.
 
-# Load preprocessed data
-data = torch.load('preprocessed_data.pth')
-esm_sequences = data['esm_sequences']
-masked_structural_sequences = data['masked_structural_sequences']
-target_structural_sequences = data['target_structural_sequences']
+def masking_seq(seq, mask_token, mask_ratio=0.15):
+    seq = list(seq)
+    seq = [seq[i] + seq[i+1] for i in range(0, len(seq), 2)]
+    num_to_mask = int(len(seq) * mask_ratio)
+    # randomly select indices from the sequence
+    mask_indices = random.sample(range(int(len(seq)/2)), num_to_mask)
+    mask_indices = [(i*2)+1 for i in mask_indices]
+    for i in mask_indices:
+        seq[i] = seq[i][0] + mask_token
+    return seq
 
-# Create models
-encoder = create_encoder(input_dim, embedding_dim, units)
-decoder = create_decoder(output_dim, embedding_dim, units)
+masking_seq(seq='AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRr', mask_token='#')
+"""
 
-# Loss function and optimizer
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
-optimizer = tf.keras.optimizers.Adam()
+def train_model():
+    """
 
-# Convert to TensorFlow Dataset
-def create_dataset(esm_sequences, masked_structural_sequences, target_structural_sequences):
-    def gen():
-        for esm_seq, masked_struct_seq, target_struct_seq in zip(esm_sequences, masked_structural_sequences, target_structural_sequences):
-            yield (esm_seq, masked_struct_seq), target_struct_seq
+    """
+    pass
 
-    return tf.data.Dataset.from_generator(
-        gen,
-        output_signature=(
-            (tf.TensorSpec(shape=(None,), dtype=tf.int32), tf.TensorSpec(shape=(None,), dtype=tf.int32)),
-            tf.TensorSpec(shape=(None,), dtype=tf.int32)
-        )
-    ).padded_batch(batch_size, padded_shapes=(([None], [None]), [None]))
+def main(confile=confile):
+    """
 
-dataset = create_dataset(esm_sequences, masked_structural_sequences, target_structural_sequences)
-
-# Training step
-@tf.function
-def train_step(input_seq, target_seq, encoder, decoder, optimizer, loss_object):
-    loss = 0
-    with tf.GradientTape() as tape:
-        encoder_input, decoder_input = input_seq
-        encoder_outputs, states = encoder(encoder_input)
-        predictions = decoder(decoder_input, initial_state=states)
-        loss = tf.reduce_mean(loss_object(target_seq, predictions))
-
-    gradients = tape.gradient(loss, encoder.trainable_variables + decoder.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, encoder.trainable_variables + decoder.trainable_variables))
+    """
     
-    return loss
+    with open(confile, 'r') as f:
+        config = json.load(f)
+    
+    timer = Timer(autoreset=True)
+    timer.start('Training started')
+    timer.stop('Training ended')
 
-# Training loop
-for epoch in range(epochs):
-    total_loss = 0
-    for (batch, (input_seq, target_seq)) in enumerate(dataset):
-        batch_loss = train_step(input_seq, target_seq, encoder, decoder, optimizer, loss_object)
-        total_loss += batch_loss
-        
-    print(f'Epoch {epoch+1} Loss {total_loss / (batch+1)}')
+if __name__ == "__main__":
+    import argparse
+    
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--config', type=str, default='config.json', help='Configuration file')
+    args = parser.parse_args()
+
+    confile = args.config
+
+    main(confile=confile)
