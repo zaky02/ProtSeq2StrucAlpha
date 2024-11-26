9681a797f3129a4bc78c4fe7d71aac1f319e40fb
Isaac Soul <isaac.soul@alumni.esci.upf.edu>
2024-11-26 15:17:21 +0100

    Created sh file to execute the training in marenostrum 5

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --account=bsc72
+#SBATCH --qos=acc_bscls     # Quality of Service or gp_bscls (gp is for cpu) acc_debug acc_bscls (acc is for gpu)
+#SBATCH --job-name=test      # Job name (change it)
+#SBATCH --output=logs/test/%j.out     # Standard output and error log (change it)
+#SBATCH --error=logs/test/%j.err        # Standard error file output (change it)
+#SBATCH --cpus-per-task=20          # Number of CPU cores per task (maximum cpus is 80, must multiply by the number of tasks)
+#SBATCH --time=12:00:00            # Time limit hrs:min:sec
+#SBATCH --gres=gpu:4                # Request 1 GPU
+#SBATCH --nodes=1                   # Request 1 node
+#SBATCH --ntasks-per-node=4                  # Request 1 task (number of tasks must be as many as GPUs)
+
+ml impi mkl intel hdf5
+source activate ProtSeq2StrucAlpha
+
+# Log in to WandB
+echo "WandB"
+wandb offline    # write it in the terminal to start wandb
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+NCCL_DEBUG=INFO
+export TIMELIMIT=12:00:00    # maximum is 48h but if we reduce the amount of hours it goes in front of other jobs 
+echo $QOS
+
+set +e
+srun ~/.conda/envs/ProtSeq2StrucAlpha/bin/python train.py --config config.json --dformat 'csv'
+set -e
+echo "Finished"
+
+# sbatch run.sh (this is to execute the file)
