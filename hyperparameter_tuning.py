d59168bbc4f8c8d2461c61fda8bdc159e4429ed4
Zaky02 <isaac.soul@alumni.esci.upf.edu>
2025-03-03 10:08:04 +0100

    Changed hyperparameter tuning to only account for the model dimensionality

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -11,57 +11,46 @@ json_dictionary = {
     "get_wandb": True,
     "wandb_project": "ProtSeq2StrucAlpha_v0",
     "epochs": 10,
+    "learning_rate": 0.0001,
+    "weight_decay": 0.0001,
+    "batch_size": 64,
     "test_split": 0.20,
     "masking_ratio": 0.15,
     "epsilon": 1e-8,
     "verbose": 1,
+    "dropout": 0.1,
     "max_len": 1024,
     "num_gpus": 4,
     "parallel_strategy": "ddp",
     "draw_model": False,
-    "early_stopping_patience": 5,
+    "early_stopping_patience": 15,
     "early_stopping_delta": 0.001
 }
 
-lr = [0.00001, 0.0001, 0.001, 0.01]
-weight_decay = [0.00001, 0.0001, 0.001, 0.01]
-dropout = [0.1, 0.3, 0.5, 0.8]
-batch_size = [64, 128, 256, 512, 1024]
+
 dim_model = [256, 512, 1024, 2048]
 ff_hidden_layer = [1024, 2048, 4096, 8192]
 num_heads = [4, 8, 16, 32]
 num_layers = [3, 6, 12, 24]
 
-for i in lr:
-    for j in weight_decay:
-        for w in dropout:
-            for z in batch_size:
-                for c in dim_model:
-                    for t in ff_hidden_layer:
-                        for m in num_heads:
-                            for n in num_layers:
-                                if c % m != 0:
-                                    continue
-                                if t % c != 0:
-                                    continue
-                                if z % json_dictionary["num_gpus"] != 0:
-                                    continue
+for c in dim_model:
+    for m in num_heads:
+        for n in num_layers:
+            for t in ff_hidden_layer:
+                if c % m != 0:
+                    continue
+                if t % c != 0:
+                    continue
 
-                                config = json_dictionary.copy()
-                                config.update({
-                                    "learning_rate": i,
-                                    "weight_decay": j,
-                                    "dropout": w,
-                                    "batch_size": z,
-                                    "dim_model": c,
-                                    "ff_hidden_layer": t,
-                                    "num_heads": m,
-                                    "num_layers": n
-                                })
+                config = json_dictionary.copy()
+                config.update({"dim_model": c,
+                               "num_heads": m,
+                               "num_layers": n,
+                               "ff_hidden_layer": t})
+                    
+                filename = f"config_dm{c}_nh{m}_nl{n}_ff{t}"
+                filename = filename.replace('.', '')
 
-                                filename = f"config_lr{i}_wd{j}_do{w}_bs{z}_dm{c}_ff{t}_nh{m}_nl{n}"
-                                filename = filename.replace('.', '')
-
-                                with open(f"configs/{filename}.json", "w") as f:
-                                    json.dump(config, f, indent=4)
+                with open(f"configs/{filename}.json", "w") as f:
+                    json.dump(config, f, indent=4)
 
