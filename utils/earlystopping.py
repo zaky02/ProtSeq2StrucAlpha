ac7b82d2d5b84d370c123d7dccbba1dc10fd6189
Zaky02 <isaac.soul@alumni.esci.upf.edu>
2025-05-13 15:58:08 +0200

    Added regular checkpoint saving of the model and optimizer at each epoch

--- utils/earlystopping.py
+++ utils/earlystopping.py
@@ -16,7 +16,7 @@ class EarlyStopping:
         self.early_stop = False
         self.verbose = verbose
         
-    def __call__(self, score, model, weights_path, fabric):
+    def __call__(self, score, model, weights_path, fabric, epoch, optimizer, checkpoint_epoch):
         
         if self.best_score - score > self.delta:
             self.best_score = score
@@ -24,16 +24,28 @@ class EarlyStopping:
                 fabric.print(f"Best evaluation loss updated to: {self.best_score:.6f}")
                 fabric.print("Saving model weights and reseting counter...")
             self.counter = 0
-            self.save_checkpoint(model, weights_path, fabric)
 
         else:
             self.counter += 1
+            self.save_checkpoint(model, weights_path, fabric, epoch, optimizer, checkpoint_epoch)
             if self.verbose > 1:
                 fabric.print(f"EarlyStopping counter: {self.counter}")
                 fabric.print(f"{self.counter}/{self.patience} till early stopping is enforced")
             if self.counter >= self.patience:
                 self.early_stop = True
+        
+        self.save_checkpoint(model, weights_path, fabric, epoch, optimizer, checkpoint_epoch)
             
-    def save_checkpoint(self, model, weights_path, fabric):
-        state = {'model': model}
+    def save_checkpoint(self, model, weights_path, fabric, epoch, optimizer, checkpoint_epoch):
+        state = {'model': model.state_dict(),
+                 'epoch': epoch,
+                 'optimizer': optimizer.state_dict(),
+                }
+
+        if checkpoint_epoch:
+            weights_path = weights_path.replace('.pth', f'_epoch_{epoch+1}.pth')
+
         fabric.save(weights_path, state)
+
+        if self.verbose:
+            fabric.print(f"Checkpoint saved: {weights_path}")
