62ec45758be644d5a044f173a5b5b53a4268cbe3
Zaky02 <isaac.soul@alumni.esci.upf.edu>
2025-05-13 15:54:53 +0200

    Changed code to handle arguments and only return average plddt score of the protein + handle empty files

--- data_preparation/plddt_score_extraction.py
+++ data_preparation/plddt_score_extraction.py
@@ -7,46 +7,41 @@ from matplotlib.collections import LineCollection
 from Bio import PDB
 
 def plldt_score_extracter(pdb_file):
+   
+    if os.path.getsize(pdb_file) == 0:
+        print(f"[WARNING] Empty PDB file skipped: {pdb_file}")
+        return 0.0 
+
     parser = PDB.PDBParser(QUIET=True)
     structure = parser.get_structure('structure', pdb_file)
     
-    residue_names = []
     plddt_scores = []
     for model in structure:
         for chain in model:
             for residue in chain:
                 for atom in residue:
-                    residue_name = f"{residue.get_resname()}_{residue.get_id()[1]}"
-                    residue_names.append(residue_name)
                     plddt_scores.append(atom.bfactor)  # B-factor stores the pLDDT score
     
-    csv_data = pd.DataFrame({'Residue': residue_names, 'pLDDT_score': plddt_scores})
-    return csv_data
-    
-def plddt_plot(csv_data, pdb_file):
-    x = np.arange(len(csv_data['pLDDT_score']))
-    y = np.array(csv_data['pLDDT Score'].tolist())
-    
-    # Create line segments and apply colormap
-    points = np.array([x, y]).T.reshape(-1, 1, 2)
-    segments = np.concatenate([points[:-1], points[1:]])
-    
-    # Set up the plot
-    fig, ax = plt.subplots(figsize=(10, 6))
-    norm = plt.Normalize(vmin=0, vmax=100)
-    
-    # Plot with color gradient
-    lc = LineCollection(segments, cmap='coolwarm_r', norm=norm, linewidth=2)
-    lc.set_array(y)
-    ax.add_collection(lc)
-    
-    # Add plot settings
-    ax.set_xlim(x.min(), x.max())
-    ax.set_ylim(0, 100)
-    ax.set_xlabel('Residue Index')
-    ax.set_ylabel('pLDDT Score')
-    plt.colorbar(lc, ax=ax, label='pLDDT Score')
-    plt.title('pLDDT Score Plot with Smooth Color Gradient')
-    plt.savefig("pdb_file[:-4]}.pdf§")
+    if not plddt_scores:
+        print(f"[WARNING] No atoms found in file: {pdb_file}")
+        return 0.0
 
+    plddt = (np.mean(plddt_scores))/100
+    print(f"The pLDDT score of the protein is: {plddt}")
+    
+    return plddt
+    
 
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Extract and plot pLDDT scores from PDB file")
+    requiredArguments = parser.add_argument_group("Required Arguments")
+    requiredArguments.add_argument('--pdb',
+                                   help="PDB file to process",
+                                   required=True)
+    args = parser.parse_args()
+
+    pdb = args.pdb
+
+    plldt_score_extracter(pdb)
